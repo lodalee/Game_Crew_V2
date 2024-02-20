9fb28ad82c7052a81ab99b913a9afde424294896
chaewon <cwlee4633@naver.com>
2024-02-20 17:07:03 +0900

    first commit

---
+++ src/main/java/com/example/game_crew_v2/GameCrewV2Application.java
@@ -0,0 +1,13 @@
+package com.example.game_crew_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameCrewV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameCrewV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/game_crew_v2/GameCrewV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.game_crew_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameCrewV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
